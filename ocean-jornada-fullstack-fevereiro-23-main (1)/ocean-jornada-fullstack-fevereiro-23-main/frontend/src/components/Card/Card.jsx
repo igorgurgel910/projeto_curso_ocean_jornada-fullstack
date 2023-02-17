12fb3d97f7c596b246c1e4c5d364dc09e9061d00
Igor Gurgel #igor910 <igor910@gmail.com>
2023-02-17 10:39:30 -0300

    update prof

---
+++ ocean-jornada-fullstack-fevereiro-23-main (1)/ocean-jornada-fullstack-fevereiro-23-main/frontend/src/components/Card/Card.jsx
@@ -0,0 +1,12 @@
+import "./Card.css"
+
+function Card() {
+    return (
+        <div className="card">
+            <h1>Rick Sanchez</h1>
+            <img src="https://rickandmortyapi.com/api/character/avatar/1.jpeg" />
+        </div>
+    )
+}
+
+export default Card;
---
+++ ocean-jornada-fullstack-fevereiro-23-main (1)/ocean-jornada-fullstack-fevereiro-23-main/frontend/src/components/ReadAll/ReadAll.jsx
@@ -0,0 +1,11 @@
+import "./ReadAll.css";
+import Card from "../Card/Card";
+
+function ReadAll() {
+    return <div className="ReadAll">
+        <Card />
+        <Card />
+    </div>
+}
+
+export default ReadAll;
